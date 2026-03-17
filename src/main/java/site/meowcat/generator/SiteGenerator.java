4f1335a22d8622b1ba1bc65d0d3e2fb91920f430
Ben <meowcat@meowcat.site>
2026-03-17 11:11:28 +0000

    site generator

--- src/main/java/site/meowcat/generator/SiteGenerator.java
+++ src/main/java/site/meowcat/generator/SiteGenerator.java
@@ -1,4 +1,31 @@
 package site.meowcat.generator;
 
+import site.meowcat.parser.MarkdownParser;
+import site.meowcat.project.DocsProject;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
 public class SiteGenerator {
+
+    private final MarkdownParser parser = new MarkdownParser();
+
+    public void build(DocsProject project) throws IOException {
+
+        Path output = project.root.resolve("build");
+        Files.createDirectories(output);
+
+        for (Path md : project.markdownFiles) {
+
+            String markdown = Files.readString(md);
+            String html = parser.toHtml(markdown);
+
+            Path outFile = output.resolve(
+                    md.getFileName().toString().replace(".md", ".html")
+            );
+
+            Files.writeString(outFile, html);
+        }
+    }
 }
