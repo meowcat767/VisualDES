d5fdc215e9f259b0573814ade1b21083e69ec974
Ben <meowcat@meowcat.site>
2026-03-17 10:16:18 +0000

    made base main and docsproject

--- src/main/java/site/meowcat/Main.java
+++ src/main/java/site/meowcat/Main.java
@@ -1,17 +1,19 @@
 package site.meowcat;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import site.meowcat.generator.SiteGenerator;
+import site.meowcat.project.DocsProject;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Path projectPath = Path.of("example-docs");
+
+        DocsProject project = DocsProject.load(projectPath);
+
+        SiteGenerator generator = new SiteGenerator();
+        generator.build(project);
+
+        System.out.println("Done!");
     }
 }
--- src/main/java/site/meowcat/project/DocsProject.java
+++ src/main/java/site/meowcat/project/DocsProject.java
@@ -1,4 +1,20 @@
 package site.meowcat.project;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class DocsProject {
+    public Path root;
+    public List<Path> markdownFiles = new ArrayList<>();
+
+    public static DocsProject load(Path root) throws IOException {
+        DocsProject project = new DocsProject();
+        project.root = root;
+
+        Files.walk(root).filter(p -> p.toString().endsWith(".md")).forEach(project.markdownFiles::add);
+        return project;
+    }
 }
