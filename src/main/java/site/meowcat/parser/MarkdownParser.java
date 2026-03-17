8bad3d621b457e8e69b81a0cceb86938c5e60b1b
Ben <meowcat@meowcat.site>
2026-03-17 10:32:18 +0000

    make a basic markdown parser

--- src/main/java/site/meowcat/parser/MarkdownParser.java
+++ src/main/java/site/meowcat/parser/MarkdownParser.java
@@ -1,4 +1,18 @@
 package site.meowcat.parser;
 
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
 public class MarkdownParser {
+    private final Parser parser;
+    private final HtmlRenderer renderer;
+
+    public MarkdownParser() {
+        this.parser = Parser.builder().build();
+        this.renderer = HtmlRenderer.builder().build();
+    }
+
+    public String toHtml(String markdown) {
+        return renderer.render(parser.parse(markdown));
+    }
 }
